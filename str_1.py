36c7111a66430a8f5eb1c08e89a92875d4703867
IFTAKHAR-ALAM <alammiftikhar@gmail.com>
2025-04-13 02:55:12 +0500

    Input and Output

--- str_1.py
+++ str_1.py
@@ -5,3 +5,13 @@ print("Hello World & Hello Github World ")
 print("I am working with Inurance. \n but I am intersted in IT")
 
 str = ("My name is iftikhat.\n I am working Insurance")
+
+int("20")
+x = int(input("Enter your val:" ))
+
+print(type(x), x))
+
+
+name = input("Enter your name:" )
+
+print("Welcoe" , name )

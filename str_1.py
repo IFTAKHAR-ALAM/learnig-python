9e1ce42c939f3085fc4f12df345b5aa76a88e3c8
IFTAKHAR-ALAM <alammiftikhar@gmail.com>
2025-04-12 17:28:24 +0500

    Update str_1.py
    Good working GitHub

--- str_1.py
+++ str_1.py
@@ -1,2 +1,7 @@
 print("Hello Github World")
+
 print("Hello World & Hello Github World ")
+
+print("I am working with Inurance. \n but I am intersted in IT")
+
+str = ("My name is iftikhat.\n I am working Insurance")

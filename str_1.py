83c7d3008d57704911e71f1e76bc38cad3e94669
IFTAKHAR-ALAM <alammiftikhar@gmail.com>
2025-04-18 10:01:17 +0500

    SET METHODS

--- str_1.py
+++ str_1.py
@@ -279,6 +279,15 @@ elif(marks >= 50):
     # collectiom.clear()
     
     
+    set1 = {1, 2, 3, 4, 5}
+    set2 = {4, 5, 6, 7, 8}
+    print(set1.union(set2))
+    print(set1.intersection(set2))
+    print(set1)
+    print(set2)
+    print(set1.difference(set2))
+    
+    
     
     
     

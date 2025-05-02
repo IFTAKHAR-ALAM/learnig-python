abbc42b8def80dddf4206049ccf7357bea057874
IFTAKHAR-ALAM <alammiftikhar@gmail.com>
2025-05-02 09:29:45 +0500

    push

--- learning-python1.py
+++ learning-python1.py
@@ -1,2 +1,10 @@
-print("Hello World & Hello Github World")
-print("My nsme is M IFTIKHAR ALAM & working in Insurance last 35 years")
+import streamlit as st
+
+st.title("Hello World")
+st.write("Hello World")
+st.write("Hello World")
+st.write("Hello World & Hello Github World")
+
+
+# print("Hello World & Hello Github World")
+# print("My nsme is M IFTIKHAR ALAM & working in Insurance last 35 years")
